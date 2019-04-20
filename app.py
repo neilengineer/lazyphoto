dc9c2ade9cd5d85f516ef3c1d1ad8ad414b497eb
Neil Zhao <neilchiao@gmail.com>
2019-04-19 23:08:18 -0700

    1)modulize lazyphoto 2)hook up flask to lazyphoto main
    todo:
    design webpage

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 from flask import Flask
+from lazyphoto import *
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/lazyphoto', methods=['GET', 'POST'])
+def process_photo():
+    upload_url = '/lazyphoto/results'
+    return render_template('lazyphoto.html', uploadurl=upload_url)
+
+
+@app.route('/lazyphoto/results', methods=['GET', 'POST'])
+def lazyphoto_results():
+    if request.method == 'POST':
+        f = request.files['file']
+        lazyphoto_process_main(f)
+
+        return render_template('lazyphoto.html', ret_photos=)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
